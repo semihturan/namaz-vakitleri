6c8ed095f09bbaa48bd1b6a7810a40074936e2b1
Semih Enver Turan <semihturan@outlook.com>
2016-01-21 16:00:45 +0200

    no message

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function() {
+  loadOptions();
+  submitHandler();
+})();
+
+function submitHandler() {
+  var $submitButton = $('#submitButton');
+
+  $submitButton.on('click', function() {
+    console.log('Submit');
+
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+  });
+}
+
+function loadOptions() {
+  var $secondsEnabledCheckbox = $('#secondsEnabledCheckbox');
+
+  if (localStorage.secondsEnabled) {
+    $secondsEnabledCheckbox[0].checked = localStorage.secondsEnabled === 'true';
+  }
+}
+
+function getAndStoreConfigData() {
+  var $secondsEnabledCheckbox = $('#secondsEnabledFormatCheckbox');
+
+  var options = {
+    secondsEnabled: $secondsEnabledCheckbox[0].checked
+  };
+
+  localStorage.secondsEnabled = options.secondsEnabled;
+
+  console.log('Got options: ' + JSON.stringify(options));
+  return options;
+}
+
+function getQueryParam(variable, defaultValue) {
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
+}
